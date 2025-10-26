f7c4c29d17505549ccb6d06c9d1e261a0cd10784
Akwasititus <www.ayamgatitus@gmail.com>
2025-10-26 12:40:33 +0000

    Add GitHub Actions CI workflow with MySQL testing

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -94,6 +94,27 @@ DATABASES = {
     }
 }
 
+# Database
+if 'GITHUB_ACTIONS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'test_db',
+            'USER': 'root',
+            'PASSWORD': 'root',
+            'HOST': '127.0.0.1',
+            'PORT': '3306',
+        }
+    }
+else:
+    # Your existing database configuration
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
