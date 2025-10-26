938f6806b42c3f8cb6cef45675e2cbf3b2383910
Akwasititus <www.ayamgatitus@gmail.com>
2025-10-26 12:09:06 +0000

    jenkinsfiles

---
+++ messaging_app/test_simple.py
@@ -0,0 +1,12 @@
+# test_simple.py
+def test_addition():
+    """A simple test that should always pass"""
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    """Another simple test"""
+    assert 5 - 3 == 2
+
+def test_always_true():
+    """This test will always pass"""
+    assert True
